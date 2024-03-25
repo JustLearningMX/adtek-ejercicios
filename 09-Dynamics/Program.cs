bd976b05a4924853ca0496c7a394f32f617593d1
Hiram Chavez <virtual.liga@gmail.com>
2024-03-24 22:29:30 -0600

    Dynamycs exercises

---
+++ 09-Dynamics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Dynamics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic d = 10;
+            Console.WriteLine(d);
+            Console.WriteLine(d.GetType());
+
+            d = "Hello";
+            Console.WriteLine(d);
+        }
+    }
+}
