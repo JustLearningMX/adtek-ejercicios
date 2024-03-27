4fe9426ef62cfaf40d8ca9a963cfc3bcc861d05b
Hiram Chavez <virtual.liga@gmail.com>
2024-03-27 10:10:13 -0600

    Value adn Reference type Exercises

--- 10-Nullables/Program.cs
+++ 10-Nullables/Program.cs
@@ -12,6 +12,11 @@ namespace _10_Nullables
         {
             Nullable<int> i = null;
             Console.WriteLine(i.HasValue);
+
+            int? j = null;
+            Console.WriteLine(j.HasValue);
+
+            int h = j ?? 0; // Si j trae un valor, se asigna a h, si no, se asigna 0
         }
     }
 }
---
+++ ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reference type
+            int x = 100;
+            Console.WriteLine("Value of x before calling changedValue() is: " + x);
+            changedValue(x);
+            Console.WriteLine("Value of x after calling changedValue() is: " + x);
+
+            //Value type
+            Student student = new Student();
+            student.ID = 1;
+            student.Name = "John";
+            Console.WriteLine("Value of student before calling changedValue() is: " + student.ID + " " + student.Name);
+            changedValue(student);
+            Console.WriteLine("Value of student after calling changedValue() is: " + student.ID + " " + student.Name);
+
+        }
+
+
+        static void changedValue(int x)
+        {
+            x = 200;
+            Console.WriteLine("Value of x in changedValue() is: " + x);
+        }
+
+        static void changedValue(Student student)
+        {
+            student.ID = 2;
+            student.Name = "Steve";
+            Console.WriteLine("Value of student in changedValue() is: " + student.ID + " " + student.Name);
+        }
+    }
+}
+
+class Student
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
