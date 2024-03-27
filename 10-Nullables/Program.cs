a80c3a6847b8b0ae0711a019b999691c57156d01
Hiram Chavez <virtual.liga@gmail.com>
2024-03-27 09:55:35 -0600

    Nullable exercises

---
+++ 10-Nullables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Nullables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> i = null;
+            Console.WriteLine(i.HasValue);
+        }
+    }
+}
