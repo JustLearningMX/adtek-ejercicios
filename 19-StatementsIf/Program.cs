82d212e6838432d935e15f7df1dfe112d2269cae
Hiram Chavez <virtual.liga@gmail.com>
2024-04-25 12:33:43 -0600

    Ejercicios de IF -ELSE IF - ELSE

---
+++ 19-StatementsIf/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_StatementsIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ejercicios IF
+             int i = 10, j = 20;
+
+            if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+
+            if (i > j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+
+            if (i > 1)
+            {
+                Console.WriteLine("i is less than j");
+            }
+
+            if (i < j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+
+            if (isGreater(i, j))
+            {
+                Console.WriteLine("i is less than j");
+            }
+
+            if (isGreater(j, i))
+            {
+                Console.WriteLine("j is greater than i");
+            }*/
+
+            /*Ejercicios IF - ELSE IF*/
+            int i = 10, j = 20;
+
+            if (i == j)
+            {
+                Console.WriteLine("i is equal to j");
+            }
+            else if (i > j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+            else if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+
+            if (i > j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+            else if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+            else
+            {
+                Console.WriteLine("i is equal to j");
+            }
+
+
+            if (i != j)
+            {
+                if (i < j)
+                {
+                    Console.WriteLine("i is less than j");
+                }
+                else if (i > j)
+                {
+                    Console.WriteLine("i is greater than j");
+                }
+            }
+            else
+                Console.WriteLine("i is equal to j");
+
+
+        }
+
+        static bool isGreater(int i, int j)
+        {
+            return i > j;
+        }
+    }
+}
---
+++ 23-ExtensionesMetodos/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _18_ExtensionesMetodos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            bool result = i.IsGreaterThan(100); //returns false 
+
+            Console.WriteLine(result);
+        }
+    }
+}
