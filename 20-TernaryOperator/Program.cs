225fa9cb02770ae49269330ed2e624f84831a15a
Hiram Chavez <virtual.liga@gmail.com>
2024-04-29 07:43:35 -0600

    Ejercicios de operaor ternario

---
+++ 20-TernaryOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_TernaryOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Example: Ternary operator
+            int x = 20, y = 10;
+
+            var result = x > y ? "x is greater than y" : "x is less than y";
+
+            Console.WriteLine(result);
+
+            // Ternary operator replaces if statement
+            // Nested ?:
+            x = 10;
+            y = 100;
+
+            result = x > y ? "x is greater than y" :
+                x < y ? "x is less than y" :
+                x == y ? "x is equal to y" : "No result";
+
+            Console.WriteLine(result);
+        }
+    }
+}
