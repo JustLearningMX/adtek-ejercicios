feaf79cb2daf526314d12e51c6963cf1345c689b
Hiram Chavez <virtual.liga@gmail.com>
2024-05-22 08:43:21 -0600

    Ejercicios de ArrayList y List

---
+++ 27-List/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_List
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2); // adding elements using add() method
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+
+            var cities = new List<string>();
+            cities.Add("New York");
+            cities.Add("London");
+            cities.Add("Mumbai");
+            cities.Add("Chicago");
+            cities.Add(null);// nulls are allowed for reference type list
+
+            //adding elements using collection-initializer syntax
+            var bigCities = new List<string>()
+            {
+                "New York",
+                "London",
+                "Mumbai",
+                "Chicago"
+            };
+
+            string[] cities2 = new string[3] { "Mumbai", "London", "New York" };
+
+            var popularCities = new List<string>();
+
+            // adding an array in a List
+            popularCities.AddRange(cities2);
+
+            var favouriteCities = new List<string>();
+
+            // adding a List 
+            favouriteCities.AddRange(popularCities);
+
+        }
+    }
+}
